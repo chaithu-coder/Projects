211c47dfe5180f8930ee9b4e1ce84dd0aa2dfa87
chaithanya dasari <92775897+chaithu-coder@users.noreply.github.com>
2024-07-12 23:00:56 +0530

    Update stack implementation using CPP.cpp

--- stack implementation using CPP.cpp
+++ stack implementation using CPP.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 #define max_size 5
 template <class type>
@@ -44,7 +44,7 @@ public:
 };
 int main()
 {
-	int op, op1, c = 0, op3, op4 = 0;
+	int op=0;int opt=0;int op1=0;int c=0;int i=0;int ch=0;int f=0;int d=0;int s=0;
 	A<int> *s1;
 	A<char> *s2;
 	A<float> *s3;
@@ -60,18 +60,21 @@ int main()
 		{
 		case 1:
 			cout << "integer data selected" << endl;
-			if (op4 == op)
+			if (i == 1)
 			{
 				cout << "continue with 1)old stack 2)new stack\n";
-				cin >> op3;
-				if (op3 == 2)
+				cin >> opt;
+				if (opt == 2)
 				{
 					delete s1;
 					s1 = new A<int>();
 				}
 			}
 			else
+			{
+				i++;
 				s1 = new A<int>();
+			}
 			while (1)
 			{
 				cout << "enter your option" << endl;
@@ -89,25 +92,29 @@ int main()
 					s1->display_stack();
 					break;
 				case 4:
-					op4 = op;
+					
 					goto l1;
 				}
 			}
 			break;
 		case 2:
 			cout << "char data selected" << endl;
-			if (op4 == op)
+			if (ch == 1)
 			{
 				cout << "continue with 1)old stack 2)new stack\n";
-				cin >> op3;
-				if (op3 == 2)
+				cin >> opt;
+				if (opt == 2)
 				{
 					delete s2;
 					s2 = new A<char>();
 				}
 			}
 			else
+			{
+				ch++;
+
 				s2 = new A<char>();
+			}	
 			while (1)
 			{
 				cout << "enter your option" << endl;
@@ -125,25 +132,28 @@ int main()
 					s2->display_stack();
 					break;
 				case 4:
-					op4 = op;
+					
 					goto l1;
 				}
 			}
 			break;
 		case 3:
 			cout << "float data selected" << endl;
-			if (op4 == op)
+			if (f == 1)
 			{
 				cout << "continue with 1)old stack 2)new stack\n";
-				cin >> op3;
-				if (op3 == 2)
+				cin >> opt;
+				if (opt == 2)
 				{
 					delete s3;
 					s3 = new A<float>();
 				}
 			}
 			else
+			{
+				f++;
 				s3 = new A<float>();
+			}
 			while (1)
 			{
 				cout << "enter your option" << endl;
@@ -161,7 +171,7 @@ int main()
 					s3->display_stack();
 					break;
 				case 4:
-					op4 = op;
+					
 					goto l1;
 				}
 			}
@@ -169,18 +179,21 @@ int main()
 		case 4:
 			cout << "double data selected" << endl;
 			s4 = new A<double>();
-			if (op4 == op)
+			if (d == 1)
 			{
 				cout << "continue with 1)old stack 2)new stack\n";
-				cin >> op3;
-				if (op3 == 2)
+				cin >> opt;
+				if (opt == 2)
 				{
 					delete s4;
 					s4 = new A<double>();
 				}
 			}
 			else
+			{
+				d++;
 				s4 = new A<double>();
+			}
 			while (1)
 			{
 				cout << "enter your option" << endl;
@@ -198,25 +211,28 @@ int main()
 					s4->display_stack();
 					break;
 				case 4:
-					op4 = op;
+					
 					goto l1;
 				}
 			}
 			break;
 		case 5:
 			cout << "string  data selected" << endl;
-			if (op4 == op)
+			if (s == 1)
 			{
 				cout << "continue with 1)old stack 2)new stack\n";
-				cin >> op3;
-				if (op3 == 2)
+				cin >> opt;
+				if (opt == 2)
 				{
 					delete s5;
 					s5 = new A<string>();
 				}
 			}
 			else
+			{
+				s++;
 				s5 = new A<string>();
+			}
 			while (1)
 			{
 				cout << "enter your option" << endl;
@@ -234,7 +250,7 @@ int main()
 					s5->display_stack();
 					break;
 				case 4:
-					op4 = op;
+					
 					goto l1;
 				}
 			}
